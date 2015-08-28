51b0aa29d50de3c4bcdfbc33f81926b4389e9fbc
Habib Pleines <habib@familiepleines.de>
2015-08-28 12:19:29 +0200

    Hilfsmethode für Übersetzung von Enum Feldern

--- fuel/application/helpers/MY_language_helper.php
+++ fuel/application/helpers/MY_language_helper.php
@@ -1,3 +1,11 @@
 <?php 
 // to simplify updates, we post the helpers in the fuel module
 require_once(FUEL_PATH.'helpers/MY_language_helper.php');
+
+function translate_enum($value) {
+    
+    switch($value) {
+        case 'yes': return 'ja'; break;
+        case 'no': return 'nein'; break;
+    }
+}
