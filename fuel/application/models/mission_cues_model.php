ab5e219d0d307dee32f9a78475271d05e566d8d8
Habib Pleines <habib@familiepleines.de>
2015-10-27 12:47:16 +0100

    Fehlerbehebung Einsatzstichwörter

--- fuel/application/models/mission_cues_model.php
+++ fuel/application/models/mission_cues_model.php
@@ -4,7 +4,7 @@ require_once('abstract_module_model.php');
 
 class Mission_Cues_model extends Abstract_module_model {
     public $unique = array('name');
-    public $required = array('name', 'description', 'aao');
+    public $required = array('name', 'description');
     
     function __construct() {
         parent::__construct('fw_mission_cues');
