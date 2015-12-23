4b2291e8a5917e7260255b2313375b48664d31b8
Habib Pleines <habib@familiepleines.de>
2015-12-23 23:21:41 +0100

    Lokalisierung

--- fuel/application/config/model.php
+++ fuel/application/config/model.php
@@ -1,14 +1,14 @@
 <?php 
 $config['auto_validate'] = TRUE;
 $config['auto_validate_fields'] = array(
-	'email|email_address' => array('valid_email', 'Please enter a valid email address.'),
-	'phone|phone_number' => array('valid_phone', 'Please enter a valid phone number.')
+	'email|email_address' => array('valid_email', 'Bitte tragen Sie eine gültige Emailadresse ein.'),
+	'phone|phone_number' => array('valid_phone', 'Bitte tragen Sie eine gültige Telefonnummer ein.')
 );
 $config['auto_date_add'] = array('date_added', 'entry_date');
 $config['auto_date_update'] = array('last_modified', 'last_updated');
 $config['date_format'] = "mm/dd/yyyy";
 $config['date_use_gmt'] = FALSE;
-$config['default_required_message'] = "Please fill out the required field '%1s'"; // uses sprintf
+$config['default_required_message'] = "Bitte füllen Sie das Pflichtfeld '%1s'"; // uses sprintf
 $config['auto_trim'] = TRUE;
 $config['auto_escape_html_entities'] = TRUE;
 
