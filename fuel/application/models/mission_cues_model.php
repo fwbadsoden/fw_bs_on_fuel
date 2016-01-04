1f2da2352915d769d713be59aa8d468a15430c34
Habib Pleines <habib@familiepleines.de>
2016-01-04 16:55:26 +0100

    Einsatzstichwörter published Status

--- fuel/application/models/mission_cues_model.php
+++ fuel/application/models/mission_cues_model.php
@@ -9,6 +9,15 @@ class Mission_Cues_model extends Abstract_module_model {
     function __construct() {
         parent::__construct('fw_mission_cues');
     }
+    
+    public function get_mission_cue_list() {
+        
+        $this->db->order_by('name asc');
+        $this->db->select('id, name');
+        $this->db->where(array('published' => 'yes'));
+        $query = $this->db->get('mission_cues');
+        return $query->result_assoc_array('id');
+    }
 }
 
 class Mission_Cue_model extends Abstract_module_record {
