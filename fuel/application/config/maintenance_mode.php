db09b2deebd302a855149ec7f8066ba1c68027d2
Habib Pleines <habib@familiepleines.de>
2016-01-22 14:57:57 +0100

    Steuerung für maintenance mode in eigene Config Datei ausgelagert. So muss nicht die Hauptkonfig angefasst werden.

---
+++ fuel/application/config/maintenance_mode.php
@@ -0,0 +1,7 @@
+<?php
+
+// set TRUE to change page to maintenance mode
+$config['offline'] = TRUE;
+
+/* End of file maintenance_mode.php */
+/* Location: ./application/config/maintenance_mode.php */
