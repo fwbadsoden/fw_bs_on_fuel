e366d6e7128e1a32e49d6d9d5b7636e68fcab5ad
Habib Pleines <habib@familiepleines.de>
2016-09-26 13:06:06 +0200

    Fehlerseite 404

--- fuel/application/views/_layouts/_mymodule.php
+++ fuel/application/views/_layouts/_mymodule.php
@@ -4,48 +4,43 @@ $param = uri_segment($segment);
 
 // check if there is a uri segment
 if ($param) {
-    
-	// if the uri segment is numeric, then search by key
-	if (is_numeric($param))	{
-		$item = fuel_model($model, array('find' => 'key', 'where' => $param));
-	}
-	else
-	{
-		// otherwise we search by find one
-		if (empty($item_where)) {
-			$item_where = array($key_field => $param);
-		}
-		$item = fuel_model($model, array('find' => 'one', 'where' => $item_where));
-	}
-	
-	// if no item is found, then do a redirect or 404 if no redirect exists
-	if (empty($item)) {
-		redirect_404();
-	}
-}
-else {
-    if(!isset($external_data)) {
-    	// if no uri segment, then we do a query on the model to get a list of data
-    	if (empty($list_where))	{
-    		$list_where = array();
-    	}
+
+    // if the uri segment is numeric, then search by key
+    if (is_numeric($param)) {
+        $item = fuel_model($model, array('find' => 'key', 'where' => $param));
+    } else {
+        // otherwise we search by find one
+        if (empty($item_where)) {
+            $item_where = array($key_field => $param);
+        }
+        $item = fuel_model($model, array('find' => 'one', 'where' => $item_where));
+    }
+
+    // if no item is found, then do a redirect or 404 if no redirect exists
+    if (empty($item)) {
+        redirect_404();
+    }
+} else {
+    if (!isset($external_data)) {
+        // if no uri segment, then we do a query on the model to get a list of data
+        if (empty($list_where)) {
+            $list_where = array();
+        }
         if (!isset($order)) {
-    	   $data = fuel_model($model, array('find' => 'all', 'where' => $list_where));
+            $data = fuel_model($model, array('find' => 'all', 'where' => $list_where));
         } else {
-    	   $data = fuel_model($model, array('find' => 'all', 'where' => $list_where, 'order' => $order));
+            $data = fuel_model($model, array('find' => 'all', 'where' => $list_where, 'order' => $order));
         }
     } else {
         $data = NULL;
     }
-} 
+}
 ?>
 
-<?php if ($item_block != NULL && !empty($item)) : ?>
-
-	<?=fuel_block(array('view' => $item_block, 'data' => $item));  ?>
-
-<?php else: ?>
-	
-	<?=fuel_block(array('view' => $list_block, 'data' => $data)); ?>
-
-<?php endif; ?>
+<?php
+if ($item_block != NULL && !empty($item)) :
+    echo fuel_block(array('view' => $item_block, 'data' => $item));
+else:
+    echo fuel_block(array('view' => $list_block, 'data' => $data));
+endif;
+?>
