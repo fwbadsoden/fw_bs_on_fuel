472ae6cd91538c23df1d3586dd9e6637df8f2571
Habib Sarbas <h.sarbas@faz.de>
2026-05-27 14:59:47 +0200

    Weitere 8.4 fixes (online validiert)

--- fuel/codeigniter/core/Model.php
+++ fuel/codeigniter/core/Model.php
@@ -46,6 +46,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @author		EllisLab Dev Team
  * @link		https://codeigniter.com/user_guide/libraries/config.html
  */
+#[\AllowDynamicProperties]
 class CI_Model {
 
 	/**
--- fuel/codeigniter/libraries/Session/Session.php
+++ fuel/codeigniter/libraries/Session/Session.php
@@ -372,7 +372,10 @@ class CI_Session {
 			{
 				// Add as many more characters as necessary to reach at least 160 bits
 				$sid_length += (int) ceil((160 % $bits) / $bits_per_character);
-				ini_set('session.sid_length', $sid_length);
+				if (PHP_VERSION_ID < 80400)
+				{
+					ini_set('session.sid_length', $sid_length);
+				}
 			}
 		}
 
